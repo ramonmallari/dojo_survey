046f47935c6095115c1e4e12c3c128ec391feaca
Ramon O. Mallari Jr <ramon.mallari@gmail.com>
2021-04-11 20:41:57 -0700

    First Commit

--- django_app/views.py
+++ django_app/views.py
@@ -2,17 +2,24 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
+  
     return render(request, 'index.html')
 
 def process(request):
-    if request.method == "POST":
-        context ={
-            'fname': request.POST['first_name'],
-            'lname': request.POST['last_name'],
-            'email': request.POST['email'],
-            'bday': request.POST['birthday'],
-            'lang': request.POST['language'],
-            'com': request.POST['comment']
-        }
-        return render(request,'results.html', context)
-    return render(request,'results.html')
+    if request.method == 'GET':
+        return redirect('/')
+    request.session['results'] = {
+        'fname': request.POST['first_name'],
+        'lname': request.POST['last_name'],
+        'email': request.POST['email'],
+        'bday': request.POST['birthday'],
+        'lang': request.POST['language'],
+        'com': request.POST['comment']
+    }
+    return redirect('/results')
+
+def result(request):
+    context = {
+        'results': request.session['results']
+    }
+    return render(request,'results.html', context)
